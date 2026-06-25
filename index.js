abe5f2782a4cb3bf3ad563af470041500eda7615
Killwa-zoldi <zoldikkilwa111@gmail.com>
2026-06-25 06:51:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,29 @@ async function startBot() {
 
   const sock = makeWASocket({
     version,
-    auth: state,
-    printQRInTerminal: true
+    auth: state
   })
 
   sock.ev.on('creds.update', saveCreds)
+
+  sock.ev.on('connection.update', async (update) => {
+    const { connection, qr } = update
+
+    if ((connection === 'connecting' || qr) && !sock.authState.creds.registered) {
+
+      const phoneNumber = '212657394310' // رقمك بدون +
+
+      const code = await sock.requestPairingCode(phoneNumber)
+
+      console.log('========================')
+      console.log('PAIRING CODE:', code)
+      console.log('========================')
+    }
+
+    if (connection === 'open') {
+      console.log('✅ تم الاتصال بنجاح')
+    }
+  })
 
   sock.ev.on('messages.upsert', async ({ messages }) => {
     const msg = messages[0]
