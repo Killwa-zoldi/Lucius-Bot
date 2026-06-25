4577f82cd53b7aa6fbb0394e345949cf59bc6489
Killwa-zoldi <zoldikkilwa111@gmail.com>
2026-06-25 06:22:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-console.log(" Lucius Bot Started");
+sock.ev.on("connection.update", async (update) => {
+  const { connection, lastDisconnect, qr } = update;
 
-setInterval(() => {
-    console.log("✅ Bot Online");
-}, 60000);
+  // QR
+  if (qr) {
+    console.log(" QR:");
+    qrcodeTerminal.generate(qr, { small: true });
+  }
+
+  // Pairing Code
+  if (!sock.authState.creds.registered) {
+    const phoneNumber = "212XXXXXXXXX"; // حط رقمك
+    const code = await sock.requestPairingCode(phoneNumber);
+    console.log(` Pairing Code: ${code}`);
+  }
+
+  if (connection === "open") {
+    console.log("✅ تم الاتصال بنجاح");
+  }
+
+  if (connection === "close") {
+    const reason = new Boom(lastDisconnect?.error)?.output?.statusCode;
+    console.log("⚠️ انقطع الاتصال");
+
+    if (reason !== DisconnectReason.loggedOut) {
+      startBot();
+    }
+  }
+});
